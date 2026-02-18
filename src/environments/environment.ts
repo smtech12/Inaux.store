f8df3a00f7f3cd4c6b5ca91f760fe925466cc26d
Shahzad Mehmood <shahzadarien@gmail.com>
2026-02-18 07:11:12 +0500

    Update environment.ts
    latest changes added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   API_BASE_URL: "https://localhost:7015/api",
   //API_BASE_URL: "https://track360-api.onrender.com/api",
-  GUEST_TOKEN_SECRET_KEY: "LOr2JpUG9N/UIIPX3Gr+vg==", // Replace with your actual secret key  
+  GUEST_TOKEN_SECRET_KEY: "LOr2JpUG9N/UIIPX3Gr+vg==", // Replace with your actual secret key 
 };
 
