4de2f78c045405b0736de25a78c68e963dfbac88
Shahzad <shahzadarien@gmail.com>
2026-02-21 03:19:46 +0500

    changes added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_BASE_URL: "http://localhost:7015/api",
+  API_BASE_URL: "https://localhost:7015/api",
   //API_BASE_URL: "https://track360-api.onrender.com/api",
   GUEST_TOKEN_SECRET_KEY: "LOr2JpUG9N/UIIPX3Gr+vg==", // Replace with your actual secret key   
 };
